3bc91e6c4fa143a924904daaeb413b67285269c8
Tarun Verghis <tarun.verghis@gmail.com>
2024-11-04 01:12:38 -0800

    Instruction Decoding

---
+++ sim.c
@@ -0,0 +1,58 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *reg_name(uint8_t reg, uint8_t w) {
+    char *reg_name_byte[8] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+    char *reg_name_word[8] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
+
+    return w == 0 ? reg_name_byte[reg] : reg_name_word[reg];
+}
+
+char *disasm_mov(uint8_t *opcode, char *buf) {
+    uint8_t d = (opcode[0] & 0x02) >> 1;
+    uint8_t w = opcode[0] & 0x01;
+    uint8_t operands = opcode[1];
+
+    uint8_t reg = (operands & 0x38) >> 3;
+    uint8_t reg_mem = (operands & 0x07);
+
+    switch (d) {
+    case 0:
+        sprintf(buf, "mov %s, %s", reg_name(reg_mem, w), reg_name(reg, w));
+        break;
+    case 1:
+        sprintf(buf, "mov %s, %s", reg_name(reg, w), reg_name(reg_mem, w));
+        break;
+    }
+
+    return buf;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("USAGE: sim <file>\n");
+        return -1;
+    }
+
+    FILE *f = fopen(argv[1], "rb");
+
+    if (f == NULL) {
+        printf("Failed to open file %s.\n", argv[1]);
+        return -1;
+    }
+
+    uint8_t buffer[2];
+    char *instr = malloc(16);
+
+    while (fread(buffer, 1, sizeof(buffer), f) != 0) {
+        instr = disasm_mov(buffer, instr);
+        printf("%s\n", instr);
+    }
+
+    free(instr);
+    fclose(f);
+
+    return 0;
+}
