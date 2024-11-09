78c7bbcfe630f04fc74307290f70afd52d320f71
Tarun Verghis <tarun.verghis@gmail.com>
2024-11-09 10:55:13 -0800

    Define bitmask macros

--- src/sim.c
+++ src/sim.c
@@ -9,13 +9,21 @@ char *REG_WORD[8] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
 
 #define REG_NAME(reg, w) ((w) == 0 ? REG_BYTE[(reg)] : REG_WORD[(reg)])
 
+#define MASK_OPCODE 0b11111100
+#define MASK_D 0b00000010
+#define MASK_W 0b00000001
+#define MASK_MOD 0b11000000
+#define MASK_REG 0b00111000
+#define MASK_RM 0b00000111
+
 void disasm_mov(uint8_t *instrs, char *buf) {
-    uint8_t d = (instrs[0] & 0b00000010) >> 1;
-    uint8_t w = instrs[0] & 0b00000001;
+    uint8_t d = (instrs[0] & MASK_D) >> 1;
+    uint8_t w = instrs[0] & MASK_W;
     uint8_t operands = instrs[1];
 
-    uint8_t reg = (operands & 0b00111000) >> 3;
-    uint8_t reg_mem = (operands & 0b00000111);
+    uint8_t mod = (operands & MASK_MOD) >> 6;
+    uint8_t reg = (operands & MASK_REG) >> 3;
+    uint8_t reg_mem = (operands & MASK_RM);
 
     switch (d) {
     case 0:
@@ -34,7 +42,7 @@ char *disasm(uint8_t *instrs) {
         return NULL;
     }
 
-    uint8_t opcode = (instrs[0] & 0b11111100) >> 2;
+    uint8_t opcode = (instrs[0] & MASK_OPCODE) >> 2;
 
     switch (opcode) {
     case 0b100010:
